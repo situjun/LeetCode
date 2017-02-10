c448b6c3f232088598b91c3cb4b1bd9c37778f0a
icewating <icewating@gmail.com>
2017-02-10 22:15:22 +0800

    1st code version

--- Leet498.1.java
+++ Leet498.1.java
@@ -1,20 +1,34 @@
 public class Solution {
-    public int numberOfBoomerangs(int[][] points) {
-        int[] multiple = new int[points.length];
-        if(points.length == 0) return 0;
-        for(int i =0;i<=points.length-1;i++) multiple[i] = (int)(Math.pow(points[i][0],2)+Math.pow(points[i][1],2));
-        //Arrays.sort(multiple);
-        ArrayList<Integer> list = new ArrayList<>();
-        int index1 = 0,index2 = multiple.length-1;
-        while(index1<index2){
-            list.add(multiple[index1]+multiple[index2]-2*points[index1][0]*points[index2][0]-2*points[index1][1]*points[index2][1]);
-            list.add(multiple[index1+1]+multiple[index2]-2*points[index1+1][0]*points[index2][0]-2*points[index1+1][1]*points[index2][1]);
-            list.add(multiple[index1]+multiple[index2-1]-2*points[index1][0]*points[index2-1][0]-2*points[index1][1]*points[index2-1][1]);
-            index1++;
-            index2--;
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if(matrix.length == 0) return new int[0];
+        int x1 = 0,y1 = 0,x2 = matrix.length-1,y2 = matrix[0].length-1;
+        int index1 = 0,index2 = 0,count = 0,sum = (x2+1)*(y2+1),tag = 0;
+        int[] result = new int[sum];
+        while(count<=sum-1){
+            //Corner 考虑，分别考虑正好抵达四条边和位于对角线上如何处理....
+            while(count<=sum-1 && index1>=0 && index2<=y2) result[count++] = matrix[index1--][index2++];
+            if(index2>y2 && index1<0){
+                index1 = 1;
+                index2 = y2;
+            } else if(index1<0){
+                index1 =0;
+            } else if(index2>y2){
+                index2 = y2;
+                index1+=2;
+            }
+            while(count<=sum-1 && index1<=x2 && index2 >=0) result[count++] = matrix[index1++][index2--];
+            if(index1>x2 && index2 <0){
+                index1=x2;
+                index2=1;
+            } else if(index1>x2){
+                index1 = x2;
+                index2 += 2;
+            } else if(index2<0){
+                index2 = 0;
+            }
+            //for(int i:result) System.out.print(i+",");
+            //System.out.print("\n");
         }
-        int result = 0;
-        for(int i:multiple) if(list.contains(new Integer(i))) result++;
         return result;
     }
 }
