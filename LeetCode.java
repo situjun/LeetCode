d45e3a7687134a988afe9ff9e4d9092850fdc490
icewating <icewating@gmail.com>
2017-03-05 20:54:42 +0800

    up

--- LeetCode.java
+++ LeetCode.java
@@ -1820,6 +1820,29 @@ public class Solution {
     }
 }
 
+535. Encode and Decode TinyURL
+public class Codec {
+    //Just cp:±³  2ÖÖ²»´íµÄsol
+    //https://discuss.leetcode.com/topic/81633/easy-solution-in-java-5-line-code
+    //https://discuss.leetcode.com/topic/81590/a-common-way-using-62-letters-java
+    List<String> urls = new ArrayList<String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        urls.add(longUrl);
+        return String.valueOf(urls.size()-1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        int index = Integer.valueOf(shortUrl);
+        return (index<urls.size())?urls.get(index):"";
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
 533. Lonely Pixel II
 public class Solution {
     public int findBlackPixel(char[][] picture, int N) {
