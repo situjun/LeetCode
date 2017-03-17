20d7323056f569df0103658867d1d95e2fa4d2db
icewating <icewating@gmail.com>
2017-03-17 20:18:33 +0800

    updated

--- LeetCode.java
+++ LeetCode.java
@@ -2903,3 +2903,54 @@ public class Solution {
         return true;
     }
 }
+
+423. Reconstruct Original Digits from English
+public class Solution {
+    public String originalDigits(String s) {
+        //Key:坑爹的题，只要找规律就行了......
+        //先找出独有的字符，再逐一排除这些数字
+        //Just cp
+        /****
+        
+        char c = s.charAt(i);
+        if (c == 'z') count[0]++;
+        if (c == 'w') count[2]++;
+        if (c == 'x') count[6]++;
+        if (c == 's') count[7]++; //7-6
+        if (c == 'g') count[8]++;
+        if (c == 'u') count[4]++; 
+        if (c == 'f') count[5]++; //5-4
+        if (c == 'h') count[3]++; //3-8
+        if (c == 'i') count[9]++; //9-8-5-6
+        if (c == 'o') count[1]++; //1-0-2-4
+        
+        ****/
+        
+        int[] count = new int[10];
+        for(char c:s.toCharArray()){
+            if (c == 'z') count[0]++;
+            if (c == 'w') count[2]++;
+            if (c == 'x') count[6]++;
+            if (c == 's') count[7]++; //7-6
+            if (c == 'g') count[8]++;
+            if (c == 'u') count[4]++; 
+            if (c == 'f') count[5]++; //5-4
+            if (c == 'h') count[3]++; //3-8
+            if (c == 'i') count[9]++; //9-8-5-6
+            if (c == 'o') count[1]++; //1-0-2-4
+        }
+        count[7] -= count[6];
+        count[5] -= count[4];
+        count[3] -= count[8];
+        count[9] = count[9] - count[8] - count[5] - count[6];
+        count[1] = count[1] - count[0] - count[2] - count[4];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 9; i++){
+            for (int j = 0; j < count[i]; j++){
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+        
+    }
+}
