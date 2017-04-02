92dbbb22de32dabc01a2097fca20efe80a0f0f13
icewating <icewating@gmail.com>
2017-04-02 17:51:26 +0800

    cp

--- LeetCode.java
+++ LeetCode.java
@@ -3022,3 +3022,29 @@ public class Solution {
         
     }
 }
+
+547. Friend Circles
+public class Solution {
+    //Key:hard
+    //Just cp https://discuss.leetcode.com/topic/85021/java-bfs-equivalent-to-finding-connected-components-in-a-graph
+    public int findCircleNum(int[][] M) {
+        int count = 0;
+        for (int i=0; i<M.length; i++)
+            if (M[i][i] == 1) { count++; BFS(i, M); }
+        return count;
+    }
+    
+    public void BFS(int student, int[][] M) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(student);
+        while (queue.size() > 0) {
+            int queueSize = queue.size();
+            for (int i=0;i<queueSize;i++) {
+                int j = queue.poll();
+                M[j][j] = 2; // marks as visited
+                for (int k=0;k<M[0].length;k++) 
+                    if (M[j][k] == 1 && M[k][k] == 1) queue.add(k);
+            }
+        }
+    }
+}
