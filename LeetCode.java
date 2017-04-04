c312bf06f9fd5b15a7ce046da04d868e713f56e1
icewating <icewating@gmail.com>
2017-04-04 19:02:45 +0800

    cp

--- LeetCode.java
+++ LeetCode.java
@@ -3265,3 +3265,32 @@ public class Solution {
     
     }
 }
+
+273. Integer to English Words
+//Key:just cp
+//https://discuss.leetcode.com/topic/25177/share-my-clean-java-solution
+public String numberToWords(int num) {
+    if (num == 0) return "Zero";
+    String[] big= {"", "Thousand", "Million", "Billion"};
+    String[] small = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    StringBuilder res = new StringBuilder();
+    int count = 0;
+    while (num != 0) {
+        int cur = num % 1000;
+        int o = cur % 10, t = (cur / 10) % 10, h = cur / 100;
+        StringBuilder tmp = new StringBuilder();
+        if (h != 0) tmp.append(ones[h] + " Hundred ");
+        if (t == 1) tmp.append(small[o] + " ");
+        else {
+            if (t > 1) tmp.append(tens[t-2] + " ");
+            if (o > 0) tmp.append(ones[o] + " ");
+        }
+        if(tmp.length() != 0) tmp.append(big[count] + " ");
+        res.insert(0, tmp);
+        num /= 1000;
+        count++;
+    }
+    return res.toString().trim();
+}
