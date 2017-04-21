03f6b28f8951472d18808d6ec31d83c971d6ac49
icewating <icewating@gmail.com>
2017-04-21 17:30:07 +0800

    cp

--- LeetCode.java
+++ LeetCode.java
@@ -4124,3 +4124,106 @@ public class Solution {
         return res;
     }
 }
+
+227. Basic Calculator II
+public class Solution {
+    public int calculate(String s) {
+        //Key:Just cp,Stack  https://discuss.leetcode.com/topic/16935/share-my-java-solution/2
+        
+        int len;
+        if(s==null || (len = s.length())==0) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        int num = 0;
+        char sign = '+';
+        for(int i=0;i<len;i++){
+            if(Character.isDigit(s.charAt(i))){
+                num = num*10+s.charAt(i)-'0';
+            }
+            if((!Character.isDigit(s.charAt(i)) &&' '!=s.charAt(i)) || i==len-1){
+                if(sign=='-'){
+                    stack.push(-num);
+                }
+                if(sign=='+'){
+                    stack.push(num);
+                }
+                if(sign=='*'){
+                    stack.push(stack.pop()*num);
+                }
+                if(sign=='/'){
+                    stack.push(stack.pop()/num);
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+    
+        int re = 0;
+        for(int i:stack){
+            re += i;
+        }
+        return re;
+    }
+}
+
+148. Sort List
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //Key:Just cp,±³  https://discuss.leetcode.com/topic/11021/basically-it-seems-like-merge-sort-problem-really-easy-understand
+    
+    //merge two sorted list, return result head
+    public ListNode merge(ListNode h1, ListNode h2){
+        if(h1 == null){
+            return h2;
+        }
+        if(h2 == null){
+            return h1;
+        }
+        
+        if(h1.val < h2.val){
+            h1.next = merge(h1.next, h2);
+            return h1;
+        }
+        else{
+            h2.next = merge(h1, h2.next);
+            return h2;
+        }
+        
+    }
+    
+    public ListNode sortList(ListNode head) {
+        //bottom case
+        if(head == null){
+            return head;
+        }
+        if(head.next == null){
+            return head;
+        }
+        
+        //p1 move 1 step every time, p2 move 2 step every time, pre record node before p1
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode pre = head;
+        
+        while(p2 != null && p2.next != null){
+            pre = p1;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        //change pre next to null, make two sub list(head to pre, p1 to p2)
+        pre.next = null;
+        
+        //handle those two sub list
+        ListNode h1 = sortList(head);
+        ListNode h2 = sortList(p1);
+        
+        return merge(h1, h2);
+        
+    }
+}
