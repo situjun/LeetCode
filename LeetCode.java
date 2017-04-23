49ff38be089c9bfdd80800597a4b873101a6b725
situjun <icewating@hotmail.com>
2017-04-23 21:47:29 +0800

    cp

--- LeetCode.java
+++ LeetCode.java
@@ -4325,3 +4325,24 @@ public class Solution {
         return stack.pop();
     }
 }
+
+322. Coin Change
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //Key:Just cp:±³ https://discuss.leetcode.com/topic/33900/java-easy-version-to-understand/2
+        if (coins == null || coins.length == 0 || amount <= 0)
+		return 0;
+    	int[] minNumber = new int[amount + 1];
+    	for (int i = 1; i <= amount; i++) {
+    		minNumber[i] = Integer.MAX_VALUE;
+    		for (int j = 0; j < coins.length; j++) {
+    			if (coins[j] <= i && minNumber[i - coins[j]] != Integer.MAX_VALUE)
+    				minNumber[i] = Integer.min(minNumber[i], 1 + minNumber[i - coins[j]]);
+    		}
+    	}
+    	if (minNumber[amount] == Integer.MAX_VALUE)
+    		return -1;
+    	else
+    		return minNumber[amount];
+    }
+}
