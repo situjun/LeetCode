3e0646ceeb5a68fb301de2745334992e7b90be70
icewating <icewating@gmail.com>
2017-05-05 17:39:21 +0800

    cp

--- LeetCode.java
+++ LeetCode.java
@@ -5280,3 +5280,96 @@ public class Solution {
     
     **/
 }
+
+41. First Missing Positive
+public class Solution {
+    //Key:cp,背
+    //https://discuss.leetcode.com/topic/10351/o-1-space-java-solution
+    public int firstMissingPositive(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+    
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
+
+43. Multiply Strings
+public class Solution {
+    public String multiply(String num1, String num2) {
+        //Key:cp,背   https://discuss.leetcode.com/topic/30508/easiest-java-solution-with-graph-explanation
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
+       
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+    
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
+            }
+        }  
+        
+        StringBuilder sb = new StringBuilder();
+        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
+57. Insert Interval
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        //cp,背  https://discuss.leetcode.com/topic/12691/short-java-code/2
+        List<Interval> result = new ArrayList<Interval>();
+        for (Interval i : intervals) {
+            if (newInterval == null || i.end < newInterval.start)
+                result.add(i);
+            else if (i.start > newInterval.end) {
+                result.add(newInterval);
+                result.add(i);
+                newInterval = null;
+            } else {
+                newInterval.start = Math.min(newInterval.start, i.start);
+                newInterval.end = Math.max(newInterval.end, i.end);
+            }
+        }
+        if (newInterval != null)
+            result.add(newInterval);
+        return result;
+    }
+}
+
+80. Remove Duplicates from Sorted Array II
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        //Key:cp,背    https://discuss.leetcode.com/topic/46519/short-and-simple-java-solution-easy-to-understand
+        //Key:同时此解法可以解决  Remove Duplicates from Sorted Array  https://leetcode.com/problems/remove-duplicates-from-sorted-array/#/description
+        
+        int i = 0;
+        for (int n : nums)
+          if (i < 2 || n > nums[i - 2])
+             nums[i++] = n;
+        return i;
+    }
+}
