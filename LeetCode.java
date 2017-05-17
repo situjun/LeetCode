4f494a9b66397ef47489032cd9930a7ac7354e54
icewating <icewating@gmail.com>
2017-05-17 17:27:53 +0800

    updated

--- LeetCode.java
+++ LeetCode.java
@@ -7837,7 +7837,7 @@ public class Solution {
 354. Russian Doll Envelopes
 public class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        //Key:just cp,mem,解法很好   https://discuss.leetcode.com/topic/47594/short-and-simple-java-solution-15-lines
+        //Key:just cp,mem,很慢的解法，估计和brute force差不多了...   https://discuss.leetcode.com/topic/47594/short-and-simple-java-solution-15-lines
         
         Arrays.sort(envelopes, (a, b) -> a[0] - b[0]);
         int max = 0;
