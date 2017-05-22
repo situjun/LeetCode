f3068f70a7ea77e4d86d935f2691decb1fe9c661
icewating <icewating@gmail.com>
2017-05-22 09:47:02 +0800

    updated

--- LeetCode.java
+++ LeetCode.java
@@ -8428,3 +8428,109 @@ public class SummaryRanges {
         return dp[sum];
     }
 }
+
+581. Shortest Unsorted Continuous Subarray
+public class Solution {
+    public int findUnsortedSubarray(int[] A) {
+        //Key:cp,mem   https://discuss.leetcode.com/topic/89282/java-o-n-time-o-1-space
+        int n = A.length, beg = -1, end = -2, min = A[n-1], max = A[0];
+        for (int i=1;i<n;i++) {
+            max = Math.max(max, A[i]);
+            min = Math.min(min, A[n-1-i]);
+            if (A[i] < max) end = i;
+            if (A[n-1-i] > min) beg = n-1-i; 
+        }
+        return end - beg + 1;
+    }
+}
+
+373. Find K Pairs with Smallest Sums
+public class Solution {
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        //Key:cp,mem     https://discuss.leetcode.com/topic/50885/simple-java-o-klogk-solution-with-explanation/2
+        PriorityQueue<int[]> que = new PriorityQueue<>((a,b)->a[0]+a[1]-b[0]-b[1]);
+        List<int[]> res = new ArrayList<>();
+        if(nums1.length==0 || nums2.length==0 || k==0) return res;
+        for(int i=0; i<nums1.length && i<k; i++) que.offer(new int[]{nums1[i], nums2[0], 0});
+        while(k-- > 0 && !que.isEmpty()){
+            int[] cur = que.poll();
+            res.add(new int[]{cur[0], cur[1]});
+            if(cur[2] == nums2.length-1) continue;
+            que.offer(new int[]{cur[0],nums2[cur[2]+1], cur[2]+1});
+        }
+        return res;
+    }
+}
+
+480. Sliding Window Median
+public class Solution {
+    //Key:cp,弃   https://discuss.leetcode.com/topic/74874/easy-to-understand-o-nlogk-java-solution-using-treemap/2
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] res = new double[nums.length-k+1];
+        TreeMap<Integer, Integer> minHeap = new TreeMap<Integer, Integer>();
+        TreeMap<Integer, Integer> maxHeap = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        
+        int minHeapCap = k/2; //smaller heap when k is odd.
+        int maxHeapCap = k - minHeapCap; 
+        
+        for(int i=0; i< k; i++){
+            maxHeap.put(nums[i], maxHeap.getOrDefault(nums[i], 0) + 1);
+        }
+        int[] minHeapSize = new int[]{0};
+        int[] maxHeapSize = new int[]{k};
+        for(int i=0; i< minHeapCap; i++){
+            move1Over(maxHeap, minHeap, maxHeapSize, minHeapSize);
+        }
+        
+        res[0] = getMedian(maxHeap, minHeap, maxHeapSize, minHeapSize);
+        int resIdx = 1;
+        
+        for(int i=0; i< nums.length-k; i++){
+            int addee = nums[i+k];
+            if(addee <= maxHeap.keySet().iterator().next()){
+                add(addee, maxHeap, maxHeapSize);
+            } else {
+                add(addee, minHeap, minHeapSize);
+            }
+            
+            int removee = nums[i];
+            if(removee <= maxHeap.keySet().iterator().next()){
+                remove(removee, maxHeap, maxHeapSize);
+            } else {
+                remove(removee, minHeap, minHeapSize);
+            }
+
+            //rebalance
+            if(minHeapSize[0] > minHeapCap){
+                move1Over(minHeap, maxHeap, minHeapSize, maxHeapSize);
+            } else if(minHeapSize[0] < minHeapCap){
+                move1Over(maxHeap, minHeap, maxHeapSize, minHeapSize);
+            }
+            
+            res[resIdx] = getMedian(maxHeap, minHeap, maxHeapSize, minHeapSize);
+            resIdx++;
+        }
+        return res;
+    }
+
+    public double getMedian(TreeMap<Integer, Integer> bigHeap, TreeMap<Integer, Integer> smallHeap, int[] bigHeapSize, int[] smallHeapSize){
+        return bigHeapSize[0] > smallHeapSize[0] ? (double) bigHeap.keySet().iterator().next() : ((double) bigHeap.keySet().iterator().next() + (double) smallHeap.keySet().iterator().next()) / 2.0;
+    }
+    
+    //move the top element of heap1 to heap2
+    public void move1Over(TreeMap<Integer, Integer> heap1, TreeMap<Integer, Integer> heap2, int[] heap1Size, int[] heap2Size){
+        int peek = heap1.keySet().iterator().next();
+        add(peek, heap2, heap2Size);
+        remove(peek, heap1, heap1Size);
+    }
+    
+    public void add(int val, TreeMap<Integer, Integer> heap, int[] heapSize){
+        heap.put(val, heap.getOrDefault(val,0) + 1);
+        heapSize[0]++;
+    }
+    
+    public void remove(int val, TreeMap<Integer, Integer> heap, int[] heapSize){
+        if(heap.put(val, heap.get(val) - 1) == 1) heap.remove(val);
+        heapSize[0]--;
+    }
+}
